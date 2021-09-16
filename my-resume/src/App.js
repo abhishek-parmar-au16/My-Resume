199b32fcdfc3c30bb7d30e7c1f148e12ad4cd904
abhishek-parmar-au16 <abhishekparmar220197@gmail.com>
2021-09-16 20:25:11 +0530

    Initial setup

---
+++ my-resume/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.min.js';
+import {BrowserRouter as Router,Route,Switch } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <>
+        <Route exact path="/" component={Home} />
+      </>
+    </Router>
+    
+  );
+}
+
+export default App;
